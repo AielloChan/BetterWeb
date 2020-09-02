2a8d29cf6558f4439796e05a2de4861e5ff584dd
Aiello Chan <aiello.chan@gmail.com>
2020-09-02 18:47:47 +0800

    Add better juejin

---
+++ better_juejin.user.js
@@ -0,0 +1,68 @@
+// ==UserScript==
+// @name         Better Juejin
+// @namespace    github.com/AielloChan/BetterWeb
+// @version      1.0
+// @description  Better Juejin view
+// @author       Aiello Chan
+// @match        *:://juejin.im/post/*
+// @grant        none
+// ==/UserScript==
+
+;(function () {
+  'use strict'
+  // Better Web Configur
+  // ################################################################
+  // better web 通用配置函数，需配合 better configurator 插件使用
+  function BetterWebConfigur(config) {
+    const NAMESPACE = 'BETTER_WEB'
+    const configurator = window.BETTER_CONFIGURATOR
+    const configuratorQuene = window.BETTER_CONFIGURATOR_QUENE || []
+    if (configurator) {
+      configurator(config)
+    } else {
+      configuratorQuene.push(config)
+      window.BETTER_CONFIGURATOR_QUENE = configuratorQuene
+    }
+    const value = localStorage.getItem(`${NAMESPACE}_${config.name}`)
+    const localValue = JSON.parse(value) || {}
+    config.options.forEach((opt) => {
+      if (localValue[opt.name] === undefined) {
+        localValue[opt.name] = opt.default
+      }
+    })
+    return localValue
+  }
+  // ################################################################
+
+  const config = BetterWebConfigur({
+    name: 'BetterZhihu',
+    label: 'Better 知乎',
+    options: [
+      {
+        name: 'fullWidth',
+        label: '文章全宽',
+        type: 'checkbox',
+        default: true,
+      },
+      {
+        name: 'hideSidebar',
+        label: '隐藏侧边栏',
+        type: 'checkbox',
+        default: true,
+      },
+    ],
+  })
+
+  const styleNode = document.createElement('style')
+
+  let cssFix = ''
+  if (config.fullWidth) {
+    cssFix += '.article-area{width:auto;}' // 答案页全宽
+  }
+  if (config.hideSidebar) {
+    cssFix += '.sidebar{display:none !important;}' // 隐藏侧边栏
+  }
+
+  styleNode.innerText = cssFix
+  document.body.append(styleNode)
+})()
