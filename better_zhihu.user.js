d0a2fecf854ac4f70966eda79bf8ba3a39e025f8
Aiello Chan <aiello.chan@gmail.com>
2021-03-22 13:08:28 +0800

    zhihu: Support zhuanlan

--- better_zhihu.user.js
+++ better_zhihu.user.js
@@ -1,27 +1,27 @@
 // ==UserScript==
 // @name         Better ZhiHu
 // @namespace    github.com/AielloChan/BetterWeb
-// @version      1.6
+// @version      1.7
 // @description  Better ZhiHu view
 // @author       Aiello Chan
 // @match        *://*.zhihu.com/*
 // @grant        none
 // ==/UserScript==
 
-;(function () {
+; (function () {
   'use strict'
-  // Better Web Configur
+  // Better Web Configure
   // ################################################################
   // better web 通用配置函数，需配合 better configurator 插件使用
-  function BetterWebConfigur(config) {
+  function BetterWebConfigure(config) {
     const NAMESPACE = 'BETTER_WEB'
     const configurator = window.BETTER_CONFIGURATOR
-    const configuratorQuene = window.BETTER_CONFIGURATOR_QUENE || []
+    const configuratorQueue = window.BETTER_CONFIGURATOR_QUEUE || []
     if (configurator) {
       configurator(config)
     } else {
-      configuratorQuene.push(config)
-      window.BETTER_CONFIGURATOR_QUENE = configuratorQuene
+      configuratorQueue.push(config)
+      window.BETTER_CONFIGURATOR_QUEUE = configuratorQueue
     }
     const value = localStorage.getItem(`${NAMESPACE}_${config.name}`)
     const localValue = JSON.parse(value) || {}
@@ -34,7 +34,7 @@
   }
   // ################################################################
 
-  const config = BetterWebConfigur({
+  const config = BetterWebConfigure({
     name: 'BetterZhihu',
     label: 'Better 知乎',
     options: [
@@ -77,6 +77,32 @@
     ],
   })
 
+  function idleSleep() {
+    return new Promise(r => requestIdleCallback(r))
+  }
+  async function waitFor(query, timeout) {
+    const startTime = Date.now()
+    while (Date.now() - startTime < timeout) {
+      const result = query()
+      if (result) {
+        return result
+      }
+      await idleSleep()
+    }
+    return null
+  }
+
+  async function removeLoginAlert() {
+    const target = await waitFor(() => {
+      try {
+        return document.querySelector(".Modal-wrapper.undefined.Modal-enter-done").parentNode.parentNode
+      } catch { }
+    }, 1e4)
+    if (target) {
+      target.remove()
+    }
+  }
+
   const href = location.href
   switch (true) {
     case /www.zhihu.com\/(question|search|hot|follow|signin|)/.test(href):
@@ -86,50 +112,82 @@
       // @match			   *://www.zhihu.com/follow
       // @match			   *://www.zhihu.com/
       // @match        *://www.zhihu.com/signin*
-      const styleNode = document.createElement('style')
-      if (config.resetTitle) {
-        setTimeout(function () {
-          document.title = '知乎'
-        }, 3e3)
-      }
+      {
+        const styleNode = document.createElement('style')
+        if (config.resetTitle) {
+          setTimeout(function () {
+            document.title = '知乎'
+          }, 3e3)
+        }
 
-      let cssFix = '.Post-content {min-width: auto !important;}'
-      if (config.fullWidth) {
-        cssFix += '.Question-mainColumn {width: 100%;}' // 答案页全宽
-      }
-      if (config.hideHeader) {
-        cssFix += 'header{display:none !important;}' // 去掉浮动头部
-      }
-      if (config.hideSidebar) {
-        cssFix += '.Question-sideColumn--sticky{display:none !important;}' // 去掉侧边栏
-      }
-      if (config.removeLoginAlert) {
-        cssFix +=
-          'html{overflow:auto !important;} ' +
-          '.Modal-wrapper {display:none !important;}' // 干掉登陆提示
-      }
+        let cssFix = 'html {overflow: auto !important;} .Post-content {min-width: auto !important;}'
+        if (config.fullWidth) {
+          cssFix += '.Question-mainColumn {width: 100%;}' // 答案页全宽
+        }
+        if (config.hideHeader) {
+          cssFix += 'header{display:none !important;}' // 去掉浮动头部
+        }
+        if (config.hideSidebar) {
+          cssFix += '.Question-sideColumn--sticky{display:none !important;}' // 去掉侧边栏
+        }
+        if (config.removeLoginAlert) {
+          // 干掉登陆提示
+          removeLoginAlert()
+        }
 
-      styleNode.innerText = cssFix
-      document.body.append(styleNode)
-      break
+        styleNode.innerText = cssFix
+        document.body.append(styleNode)
+        break
+      }
+    case /zhuanlan.zhihu.com\/p\/*/.test(href):
+      // @match        *://zhuanlan.zhihu.com/p/*
+      {
+        const styleNode = document.createElement('style')
+        if (config.resetTitle) {
+          setTimeout(function () {
+            document.title = '知乎'
+          }, 3e3)
+        }
+
+        let cssFix = 'html {overflow: auto !important;} .Post-content {min-width: auto !important;}'
+        if (config.fullWidth) {
+          cssFix += '.Question-mainColumn {width: 100%;}' // 答案页全宽
+        }
+        if (config.hideHeader) {
+          cssFix += 'header{display:none !important;}' // 去掉浮动头部
+        }
+        if (config.hideSidebar) {
+          cssFix += '.Question-sideColumn--sticky{display:none !important;}' // 去掉侧边栏
+        }
+        if (config.removeLoginAlert) {
+          // 干掉登陆提示
+          removeLoginAlert()
+        }
+
+        styleNode.innerText = cssFix
+        document.body.append(styleNode)
+        break
+      }
     case /link.zhihu.com/.test(href):
       // @match        *://link.zhihu.com/*
-      function getQueryVariable(name) {
-        const reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i')
-        const result = window.location.search.substr(1).match(reg)
-        if (result != null) {
-          return decodeURI(result[2])
-        } else {
-          return null
+      {
+        function getQueryVariable(name) {
+          const reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i')
+          const result = window.location.search.substr(1).match(reg)
+          if (result != null) {
+            return decodeURI(result[2])
+          } else {
+            return null
+          }
         }
-      }
-      if (config.directlyRoute) {
-        const target = getQueryVariable('target')
-        if (target !== null) {
-          const directLink = window.unescape(target)
-          window.location.href = directLink
+        if (config.directlyRoute) {
+          const target = getQueryVariable('target')
+          if (target !== null) {
+            const directLink = window.unescape(target)
+            window.location.href = directLink
+          }
         }
+        break
       }
-      break
   }
 })()
